d5948c3a4f84e5528662dfc404c2bcf017d6cadf
Rachael Oyelowo <rachaeloyelowo123@gmail.com>
2024-11-18 22:33:13 -0500

    remove duplicates file created, remove duplicates attempt added

---
+++ RemoveDuplicates.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicates{
+    public int removeDuplicates(int[] nums) {
+        
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                
+            }
+        }
+
+        return count;
+    }
+}
